8358b652ef037b7205c7ca9de5695832359c63c7
Alessio <ale79631@gmail.com>
2021-02-24 21:22:59 +0100

    miglioramenti

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ var cognomi_due;
 
 cognomi.push(prompt("Inserisci il tuo cognome all'elenco, inserisci solo lettere minuscole."));
 
-
-for (i = 0; i < cognomi.length; i++) {
-    console.log(cognomi[i]);
-}
+console.log(cognomi);
 
 var num_utenti = cognomi.length;
 var nuovo_utente = cognomi[num_utenti - 1];
@@ -18,7 +15,7 @@ cognomi_due = cognomi;
 for (i = 0; i < cognomi_due.length; i++) {
 
     for (ii = i + 1; ii < cognomi_due.length; ii++) { 
-        
+
         if (cognomi_due[i] > cognomi_due[ii]) {
             var r = cognomi_due[ii];
             cognomi_due[ii] = cognomi_due[i];
@@ -26,13 +23,13 @@ for (i = 0; i < cognomi_due.length; i++) {
         }
 
     }
-
 }
 
 for (i = 0; i < cognomi_due.length; i++) {
     document.getElementById("lista").innerHTML += "<li>" + cognomi_due[i] + "</li>";
+
     if (nuovo_utente == cognomi_due[i]) {
         alert(cognomi_due[i] + ", posizione nr. " + (i + 1) + ".");
-        
     } 
+
 }
